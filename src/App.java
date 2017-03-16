d54b5d929eca777621630e59da9bedae2590f48c
Yakkodash <al.milos@mail.ru>
2017-03-17 01:26:54 +0300

    Fixed test messages

--- src/App.java
+++ src/App.java
@@ -13,30 +13,30 @@ public class App
 
         long startTime = System.nanoTime();
         Ex1 govno = new Ex1();
-        BigDecimal bg = govno.getEuler(300);
+        BigDecimal bg = govno.getEuler(300 );
         System.out.println( bg);
         long endTime = System.nanoTime();
-        System.out.println("GOVNO 300"+ (endTime - startTime)/1000000);
+        System.out.println("GOVNO 300 "+ (endTime - startTime)/1000000);
        /* for (int i=100;i<=100;i++) {
           bg = govno.getEuler(i);
           System.out.println( bg);
         }*/
         startTime = System.nanoTime();
-        bg = EulerNum.calculate(300);
+        bg = EulerNum.calculate(300 );
         System.out.println( bg);
         endTime = System.nanoTime();
-        System.out.println("NASHE GOVNO 300"+ (endTime - startTime)/1000000);
+        System.out.println("NASHE GOVNO 300 "+ (endTime - startTime)/1000000);
 
         startTime = System.nanoTime();
         bg = govno.getEuler(100);
         System.out.println( bg);
         endTime = System.nanoTime();
-        System.out.println("GOVNO 100"+ (endTime - startTime)/1000000);
+        System.out.println("GOVNO 100 "+ (endTime - startTime)/1000000);
 
         startTime = System.nanoTime();
-        bg = EulerNum.calculate(300);
+        bg = EulerNum.calculate(300 );
         System.out.println( bg);
         endTime = System.nanoTime();
-        System.out.println("NASHE GOVNO 100"+ (endTime - startTime)/1000000);
+        System.out.println("NASHE GOVNO 100 "+ (endTime - startTime)/1000000);
     }
 }
