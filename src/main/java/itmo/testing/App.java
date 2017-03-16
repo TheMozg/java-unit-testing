9ced0631360e1d003345d484e9299ec422f0affb
Fedor Kalugin <fedor.md@gmail.com>
2017-03-16 14:50:36 +0000

    Create Maven project

---
+++ src/main/java/itmo/testing/App.java
@@ -0,0 +1,13 @@
+package itmo.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
